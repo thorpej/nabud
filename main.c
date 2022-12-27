412b628eeae77dfd210522eb2763b75fd400da03
Jason Thorpe <thorpej@me.com>
2022-12-26 16:29:26 -0800

    Ok, most of the basic server stuff is fleshed-out now.

--- main.c
+++ main.c
@@ -28,7 +28,12 @@
  * NaturalAccess -- a server for the NABU PC.
  */
 
+#include <errno.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "adaptor.h"
@@ -36,8 +41,119 @@
 #include "log.h"
 #include "segment.h"
 
+#ifndef DEFAULT_NABU_SEGMENTS_DIR
+#define	DEFAULT_NABU_SEGMENTS_DIR	"./nabu_segments"
+#endif
+
+static int	debug = true;
+
+static void __attribute__((__noreturn__))
+usage(void)
+{
+	fprintf(stderr, "usage: %s [-d] [-s segments_dir] [tty_path [...]]\n",
+	    getprogname());
+	exit(EXIT_FAILURE);
+}
+
+static void *
+connection_thread(void *arg)
+{
+	struct nabu_connection *conn = arg;
+
+	/*
+	 * Just run the Adaptor event loop until it returns.
+	 */
+	adaptor_event_loop(conn);
+	return NULL;
+}
+
 int
 main(int argc, char *argv[])
 {
+	const char *segdir = DEFAULT_NABU_SEGMENTS_DIR;
+	int ch;
+
+	setprogname(argv[0]);
+
+	while ((ch = getopt(argc, argv, "ds:")) != -1) {
+		switch (ch) {
+		case 'd':
+			debug = true;
+			break;
+
+		case 's':
+			segdir = optarg;
+			break;
+
+		default:
+			usage();
+			/* NOTREACHED */
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (! segment_init(segdir)) {
+		/* Error already logged. */
+		exit(EXIT_FAILURE);
+	}
+
+	/* Set up our signal state. */
+	(void) signal(SIGHUP, SIG_IGN);
+	(void) signal(SIGPIPE, SIG_IGN);
+	(void) signal(SIGINT, SIG_IGN);
+	(void) signal(SIGTERM, SIG_IGN);
+
+	/*
+	 * For each tty_path, create a serial connection and create a
+	 * thread to service it.
+	 */
+	struct nabu_connection *conn;
+	pthread_t thread;
+	pthread_attr_t attr;
+	int error;
+
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+	for (; argc != 0; argc--, argv++) {
+		log_info("Creating serial connection on %s.", *argv);
+		conn = conn_create_serial(*argv);
+		if (conn == NULL) {
+			/* Not fatal.  Error already logged. */
+			continue;
+		}
+		error = pthread_create(&thread, &attr, connection_thread, conn);
+		if (error) {
+			log_error("pthread_create() for %s failed: %s",
+			    *argv, strerror(error));
+			abort();
+			/* NOTREACHED */
+		}
+	}
+
+	if (conn_count == 0) {
+		log_error("No connections! So boring! Goodbye.");
+		exit(EXIT_FAILURE);
+	}
+
+	/*
+	 * Now that our connections are up and running, just wait
+	 * for a clean-shutdown signal.
+	 */
+	sigset_t waitset;
+	int sig;
+
+	sigemptyset(&waitset);
+	sigaddset(&waitset, SIGINT);
+	sigaddset(&waitset, SIGTERM);
+
+	if (sigwait(&waitset, &sig) != 0) {
+		log_fatal("sigwait() failed: %s\n", strerror(errno));
+		/* NOTREACHED */
+	}
+
+	log_info("Exiting on signal %d.", sig);
+	/* XXX Shut down all connections. */
 	exit(0);
 }
